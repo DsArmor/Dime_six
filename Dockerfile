5a7814db2f79136427ebc3751a4159566923a021
DsArmor <zeleznoff84-00@mail.ru>
2023-05-23 21:27:26 +0300

    исправление docker-compose.yml

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,12 @@
 FROM maven:3.8.5-openjdk-18 AS build
 COPY src /home/app/src
 COPY pom.xml /home/app
-COPY .openapi-generator-ignore /home/app
-RUN mvn -f /home/app/pom.xml clean package
+RUN mvn -f /home/app/pom.xml clean package -Dmaven.test.skip
 
 #
 # Package stage
 #
 FROM openjdk:18
-COPY --from=build /home/app/target/spring-boot-docker-maven.jar /usr/local/lib/trpp.jar
+COPY --from=build /home/app/target/booking-0.0.1.jar /usr/local/lib/booking.jar
 EXPOSE 8080
-CMD ["java", "-jar", "/usr/local/lib/trpp.jar"]
+CMD ["java", "-jar", "/usr/local/lib/booking.jar"]
--- frontend/src/components/Booking.jsx
+++ frontend/src/components/Booking.jsx
@@ -1,20 +1,9 @@
 import * as React from 'react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 
 const Booking = (props) => {
   return (
-
 		<TableRow>
 			<TableCell component="th" scope="row">
 				{props.booking.room.name}
